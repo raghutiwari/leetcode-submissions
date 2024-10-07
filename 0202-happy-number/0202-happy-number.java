724cf21488e26b0981ac02fe82b64303d4612939
Raghu Tiwari <raghutiwari12345@gmail.com>
2024-10-07 21:42:53 +0530

    Time: 0 ms (100%), Space: 40.6 MB (64.55%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = square(slow);
+            fast = square(square(fast));
+        } while (slow != fast) ;
+
+        return slow == 1;
+        
+    }
+
+    public static int square(int n) {
+        int ans = 0;
+
+        while (n > 0) {
+            int remainder = n % 10;
+            ans += remainder * remainder;
+            n /= 10;
+        }
+        return ans;
+    }
+}

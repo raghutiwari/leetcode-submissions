3b5a92ffecb588046edc9e344e89cb836b1f26f0
Raghu Tiwari <raghutiwari12345@gmail.com>
2024-10-09 21:53:27 +0530

    Time: 0 ms (100%), Space: 41.5 MB (45.45%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,31 +1,50 @@
 class Solution {
-    public int minAddToMakeValid(String s) {
-        Stack<Character> stack = new Stack<>();
 
+    // Without using extra space
+
+    public int minAddToMakeValid(String s) {
         char[] charArr = s.toCharArray();
-        int counter = 0;
+        int open = 0;
+        int close = 0;
         for (int i = 0; i < charArr.length; i++) {
             if (charArr[i] == '(') {
-                stack.push(charArr[i]);
-            }
-
-            if (charArr[i] == ')') {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                } else {
-                    counter++;
-                }
+                open++;
+            } else if (charArr[i] == ')' && open > 0) {
+                open--;
+            } else {
+                close++;
             }
         }
 
-        while (!stack.isEmpty()) {
-            stack.pop();
-            counter++;
-        }
-
-        return counter;
-
-
-
+        return open + close;
     }
+
+    // With using extra space
+    // public int minAddToMakeValid(String s) {
+    //     Stack<Character> stack = new Stack<>();
+
+    //     char[] charArr = s.toCharArray();
+    //     int counter = 0;
+    //     for (int i = 0; i < charArr.length; i++) {
+    //         if (charArr[i] == '(') {
+    //             stack.push(charArr[i]);
+    //         }
+
+    //         if (charArr[i] == ')') {
+    //             if (!stack.isEmpty()) {
+    //                 stack.pop();
+    //             } else {
+    //                 counter++;
+    //             }
+    //         }
+    //     }
+
+    //     while (!stack.isEmpty()) {
+    //         stack.pop();
+    //         counter++;
+    //     }
+
+    //     return counter;
+
+    // }
 }
